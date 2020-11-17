b5de915516a355c4602c4310b53fc49de11bb6be
Federico Ramirez <federico_r@beezwax.net>
2020-11-17 17:17:01 -0300

    Tests

--- src/TodoApp.test.js
+++ src/TodoApp.test.js
@@ -1,8 +1,15 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import userEvent from '@testing-library/user-event';
+import TodoApp from './TodoApp';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('can add a new TODO', () => {
+  render(<TodoApp />);
+
+  const input = screen.getByPlaceholderText('New Todo');
+  input.value = "A demo TODO"
+
+  userEvent.click(screen.getByText('Add'))
+
+  expect(screen.getByText('A demo TODO')).toBeInTheDocument()
+  expect(input.value).toBe('')
 });
